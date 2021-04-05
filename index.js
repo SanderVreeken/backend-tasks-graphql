aae0a164a2171e5499d3187931f2fffdb1e6629c
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Pro.local>
2021-04-05 13:03:08 +0200

    Added an extra allowed domain to CORS.

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const server = new ApolloServer({
 server.applyMiddleware({
     app,
     cors: {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://next-tasks-typescript-bnlfmhrn7-sandervreeken.vercel.app'],
         credentials: true,
     }
 })
